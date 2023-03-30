4dc6125962c22d1b8ebd8628064768ebea9f0b96
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-03-30 16:27:43 +0200

    Update

--- main.py
+++ main.py
@@ -1,13 +1,91 @@
-from random import random
-from glob import glob
+from telegram.ext import * 
+import telegram
+
+'''
+
+0 - aspetta il comando start 
+1 - chiede se si vuole info o se si vuole mandare un messaggio 
+2 - chiede a che numero si vuole mandare il messaggio
+'''
 
 
+phase = 0
 
 def retrieveToken(filename):
     token = open(filename, 'r').read()
     return token
 
+def write_to_channel(update, context, text):
+    # Prendi il testo del messaggio inviato dopo il comando /scrivi
+
+    # Invia il messaggio di testo nel canale "iGenLoveMeParty"
+    context.bot.send_message(chat_id="@LovifyChannel", text=text)
+
+
+
+
+def handle_text(update, context):
+    global phase0Message, notintMessage, toNumber, fromNumber, fromAcceptedMessage, toAcceptedMessage, messageSentMessage, phase, message
+    text = update.message.text
+    if(phase == 1):
+        context.bot.send_message(chat_id = update.effective_chat.id, text=phase0Message)
+    elif(phase == 2):
+        if(not text.isnumeric()):
+            context.bot.send_message(chat_id = update.effective_chat.id, text=notintMessage)
+        else:
+            context.bot.send_message(chat_id = update.effective_chat.id, text=toAcceptedMessage)
+            toNumber = int(text)
+            phase = 3
+    elif(phase == 3):
+        if(not text.isnumeric()):
+            context.bot.send_message(chat_id = update.effective_chat.id, text=notintMessage)
+        else:
+            context.bot.send_message(chat_id = update.effective_chat.id, text=fromAcceptedMessage)
+            fromNumber = int(text)
+            phase = 4
+    elif(phase == 4):
+        message = text
+        context.bot.send_message(chat_id = update.effective_chat.id, text=messageSentMessage)
+        text = "{} ha scritto a {} il messaggio -> {}".format(fromNumber, toNumber, message)
+        write_to_channel(update, context, text)
+        phase = 1
+
+def start(update, context):
+    global phase, initMessage
+    phase = 1
+    context.bot.send_message(chat_id=update.effective_chat.id, text=initMessage)
+
+def asktoNumber(update, context):
+    global phase2Message, phase
+    phase = 2
+    print("Entrato")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=phase2Message)
+
+toNumber = 0
+fromNumber = 0
+message = ""
 
 token = retrieveToken("token.txt")
+initMessage = open('initMessage.txt', 'r').read()
+phase0Message = open('phase0message.txt', 'r').read()
+phase2Message = open("phase2Message.txt", 'r').read()
+notintMessage = open("notintmessage.txt", 'r').read()
+fromAcceptedMessage = open("fromAcceptedMessage.txt", 'r').read()
+toAcceptedMessage = open("toAcceptedMessage.txt", "r").read()
+messageSentMessage = open("messageSent.txt", "r").read()
 
-updater = Updater(token)
+updater = Updater(token=token, use_context=True)
+dispatcher = updater.dispatcher
+
+#dispatcher.add_handler(MessageHandler(Filters.text, handle_text))
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("inviaunmessaggio", asktoNumber))
+
+dispatcher.add_handler(MessageHandler(Filters.text, handle_text))
+
+
+
+
+
+print("Startin the bot")
+updater.start_polling(1.0)
