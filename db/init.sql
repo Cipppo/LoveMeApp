e73e2213c97aa93ea0b41e0972231ae9c28dff4c
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-04-13 10:27:15 +0200

    Update database

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE IF NOT EXISTS lovify;
 USE lovify;
 CREATE TABLE messaggi (
-    ID int,
-    sender varchar(255),
+    ID int NOT NULL AUTO_INCREMENT,
     receiver varchar(255),
+    sender varchar(255),
     textSent varchar(255), 
-    msgRead varchar(255)
-);
+    msgRead varchar(255), 
+    PRIMARY KEY(ID)
+) AUTO_INCREMENT = 100 ;
